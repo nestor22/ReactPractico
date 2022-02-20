5b43df86413be7032c1774edeb390a1b0a86c0e8
ngonzalez <nestor.ge222@gmail.com>
2022-02-19 22:19:52 -0600

    configuracion de layout y estilos

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import '../styles/login.scss';
 
 const Login = () => {
   return (
     <div className="login">
       <div className="form-container">
         <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-
         <h1 className="title">Create a new password</h1>
         <p className="subtitle">Enter a new passwrd for yue account</p>
-
         <form action="/" className="form">
           <label for="password" className="label">
             Password
@@ -19,7 +18,6 @@ const Login = () => {
             placeholder="*********"
             className="input input-password"
           />
-
           <label for="new-password" className="label">
             Password
           </label>
@@ -29,7 +27,6 @@ const Login = () => {
             placeholder="*********"
             className="input input-password"
           />
-
           <input
             type="submit"
             value="Confirm"
