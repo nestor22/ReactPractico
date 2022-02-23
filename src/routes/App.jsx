3dc1eff4146a334823f06ee0b51d1ee5847e706f
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-02-22 22:47:05 -0600

    actualizacion de home component y not found

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
 import RecoveryPassword from "../containers/recoveryPassoword";
+import Home from "../pages/Home";
+import NotFound from "../pages/NotFound";
 import "../styles/global.css";
 
 const App = () => {
@@ -12,7 +14,7 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/recovery" element={<RecoveryPassword />} />
-          <Route exact path="/" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
