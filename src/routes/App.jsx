2a438475da85a98cfec105393e3ddb8304666650
ngonzalez <nestor.ge222@gmail.com>
2022-02-26 21:58:04 -0600

    configuracion de paginas y componentes de proyecto anterior

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,7 +4,13 @@ import Layout from "../containers/Layout";
 import Login from "../containers/Login";
 import RecoveryPassword from "../containers/recoveryPassoword";
 import Home from "../pages/Home";
+import SendEmail from "../pages/SendEmail";
 import NotFound from "../pages/NotFound";
+import MyAccount from "../pages/MyAccount";
+import CreateAccount from "../pages/CreateAccount";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
+import NewPassword from "../pages/NewPassword";
 import "../styles/global.css";
 
 const App = () => {
@@ -15,6 +21,13 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/recovery" element={<RecoveryPassword />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/send-email" element = {<SendEmail />} />
+          <Route exact path="/new-password" element={<NewPassword />} />
+          <Route exact path="/account" element={<MyAccount />} />
+          <Route exact path="/signup" element={<CreateAccount />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+          <Route exact path="/orders" element={<Orders />} />
+
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Layout>
