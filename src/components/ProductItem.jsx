24b40dcb15d7c56c094661767ab4addfdf692c83
ngonzalez <nestor.ge222@gmail.com>
2022-03-06 15:36:18 -0600

    add hooks pesonales a productos

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,29 +1,31 @@
-import React, {useState} from 'react';
-import '../styles/ProductItem.scss';
-import addToCart from '@icons/bt_add_to_cart.svg'
+import React, { useState } from "react";
+import "../styles/ProductItem.scss";
+import addToCart from "@icons/bt_add_to_cart.svg";
 
-const ProductItem = () => {
-	const [cart, setCart] = useState([]);
+const ProductItem = ({product}) => {
+  const [cart, setCart] = useState([]);
 
-	const handleCart = () =>{
-		setCart([]);
-	}
+  const handleCart = () => {
+    setCart([]);
+  };
 
-	return (
-		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
-			<div className="product-info">
-				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
-				</div>
-				<figure onClick = {handleCart}>
-					<img src={addToCart} alt="" />
-				</figure>
-				
-			</div>
-		</div>
-	);
-}
+  return (
+    <div className="ProductItem">
+      <img
+        src={product.images[0]} 
+        alt={product.title}
+      />
+      <div className="product-info">
+        <div>
+          <p>${product.price}</p>
+          <p>{product.title}</p>
+        </div>
+        <figure onClick={handleCart}>
+          <img src={addToCart} alt="" />
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 export default ProductItem;
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,30 +1,23 @@
-import React, {useEffect, useState } from 'react';
-import ProductItem from '../components/ProductItem';
-import '../styles/ProductList.scss';
-import axios from 'axios';
+import React from "react";
+import ProductItem from "../components/ProductItem";
+import "../styles/ProductList.scss";
+import useGetProducts from "@hooks/useGetProducts.js";
 
-const API = 'http://api.escuelajs.co/api/v1/products';
 
+
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
-	const [products, setProducts]=useState([]);
-
-	useEffect(()=>{
-		
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])
-
-
-	return (
-		<section className="main-container">
-			<div className="ProductList">
-				{products.map(product => (
-					<ProductItem />
-				))}
-			</div>
-		</section>
-	);
-}
+  const products = useGetProducts(API);
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        {products.map((product) => (
+          <ProductItem product={product} key={product.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
