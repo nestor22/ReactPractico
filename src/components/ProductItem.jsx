043f1ad884ece9f31c21b8056b95c3a980f1f6ef
ngonzalez <nestor.ge222@gmail.com>
2022-03-07 20:31:01 -0600

    add app context and functional cart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "../styles/ProductItem.scss";
+import AppContext from "../context/AppContext";
 import addToCart from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = ({product}) => {
-  const [cart, setCart] = useState([]);
+ 
+  const { addToCart } = useContext(AppContext);
 
-  const handleCart = () => {
-    setCart([]);
+  const handleClick = item => {
+    addToCart(item);
   };
 
   return (
@@ -20,7 +22,7 @@ const ProductItem = ({product}) => {
           <p>${product.price}</p>
           <p>{product.title}</p>
         </div>
-        <figure onClick={handleCart}>
+        <figure onClick={()=>handleClick(product)}>
           <img src={addToCart} alt="" />
         </figure>
       </div>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,27 +11,31 @@ import CreateAccount from "../pages/CreateAccount";
 import Checkout from "../pages/Checkout";
 import Orders from "../pages/Orders";
 import NewPassword from "../pages/NewPassword";
+import AppContext from "../context/AppContext";
 import "../styles/global.css";
-
+import useInitialState from "../hooks/useInitialState";
 const App = () => {
+  const initialState = useInitialState();
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/recovery" element={<RecoveryPassword />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/send-email" element = {<SendEmail />} />
-          <Route exact path="/new-password" element={<NewPassword />} />
-          <Route exact path="/account" element={<MyAccount />} />
-          <Route exact path="/signup" element={<CreateAccount />} />
-          <Route exact path="/checkout" element={<Checkout />} />
-          <Route exact path="/orders" element={<Orders />} />
+    <AppContex.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/recovery" element={<RecoveryPassword />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/send-email" element={<SendEmail />} />
+            <Route exact path="/new-password" element={<NewPassword />} />
+            <Route exact path="/account" element={<MyAccount />} />
+            <Route exact path="/signup" element={<CreateAccount />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route exact path="/orders" element={<Orders />} />
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContex.Provider>
   );
 };
 
